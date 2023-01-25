6ce9f3c4b2d323121cd4f69d953078c04792277b
metagenesis-nowsheturns <maria-proenca@edu.ulisboa.pt>
2023-01-25 09:10:55 +0000

    567

---
+++ myscript.js
@@ -0,0 +1,25 @@
+const ImgNumber = {
+    1: "img/1.jpg",
+    2: "img/2.jpg",
+    3: "img/3.jpg", }
+    
+
+function setFigureProperties(imgNumber) {
+    let figure = document.getElementById("hoverImageContainer");
+    figure.style.display = "block";
+    let img = document.getElementById("hoverImage");
+    img.src = ImgNumber[imgNumber];
+    let caption = document.getElementById("caption");
+    caption.textContent = imgCaption;
+    
+    
+}
+function hideImage(){
+    let figure = document.getElementById("hoverImageContainer");
+    figure.style.display = "none";
+}
+
+
+
+
+
